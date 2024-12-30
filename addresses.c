9c8a23fc56b1c9ec82468c8adfa878c4adb368f5
thales0591 <thales0591@gmail.com>
2024-12-29 21:03:06 -0300

    update most recent done exercises

--- addresses.c
+++ addresses.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
-    string s = "HI!";
-    printf("%p\n", s);
-    printf("%p\n", &s[0]);
+    char *s = "HI!";
+    printf("%c", s[0]);
+     printf("%c", s[1]);
+    printf("%c\n", s[2]);
 }
+
