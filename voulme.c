0a400c03ed8e5e661d917e31f523dade0775f5f4
thales0591 <thales0591@gmail.com>
2025-01-05 20:50:43 -0300

    update most recent done exercises

--- voulme.c
+++ voulme.c
@@ -17,14 +17,14 @@ int main(int argc, char *argv[])
     }
 
     // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "r");
+    FILE *input = fopen(argv[1], "rb");
     if (input == NULL)
     {
         printf("Could not open file.\n");
         return 1;
     }
 
-    FILE *output = fopen(argv[2], "w");
+    FILE *output = fopen(argv[2], "wb");
     if (output == NULL)
     {
         printf("Could not open file.\n");
@@ -34,8 +34,28 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    uint8_t *b = malloc(HEADER_SIZE);
+    if (b == NULL)
+    {
+        return 1;
+    }
+    fread(b, HEADER_SIZE, 1, input);
+    fwrite(b, HEADER_SIZE, 1, output);
+    free(b);
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t *b2 = malloc(sizeof(int16_t));
+    if (b2 == NULL)
+    {
+        return 1;
+    }
+    int counter = 0;
+    while (fread(b2, sizeof(int16_t), 1, input) != 0)
+    {
+        int16_t truncatedInt = (int16_t) (*b2 * factor);
+        *b2 = truncatedInt;
+        fwrite(b2, sizeof(int16_t), 1, output);
+    }
 
     // Close files
     fclose(input);
